865995edfd742eb40c28b27f967caf61f68fad8f
Sharad2001 <57340172+Sharad2001@users.noreply.github.com>
2022-02-25 12:18:53 +0530

    Robots
    There are two kinds of bots A and B in a 1-D array. A bot can only move left while B can only move right. There are also empty spaces in between represented by #. But its also given that the bots cannot cross over each other.
    
    Given the initial state and final state, we should tell if the transformation is possible.

---
+++ robots.py
@@ -0,0 +1,21 @@
+class Solution:
+    def moveRobots(self, s1, s2):
+        if len(s1) != len(s2):
+            return "No"
+        bots = []
+        for i in range(len(s1)):
+            if s1[i] != "#":
+                bots.append(i)
+        b = 0
+        for j in range(len(s2)):
+            if s2[j] != "#":
+                if b == len(bots) or s2[j] != s1[bots[b]] or (s2[j] == "A" and j > bots[b]) or (
+                        s2[j] == "B" and j < bots[b]):
+                    return "No"
+                b += 1
+        return "Yes" if b == len(bots) else "No"
+if __name__ == '__main__':
+    s1 = input()
+    s2 = input()
+    ob = Solution()
+    print(ob.moveRobots(s1, s2))
