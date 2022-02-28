72dbcefa38297493ae081e8d2103373974e4bea5
Sharad2001 <57340172+Sharad2001@users.noreply.github.com>
2022-02-28 13:42:51 +0530

    Hit most number of balloons
    You are given an infinite two-dimensional grid. There are N balloons placed at certain coordinates of the grid. You have an arrow with yourself, which you will be using to shoot the balloons. You can select any point on the grid as your starting point and any point on the grid as the target point. When you fire the arrow, all balloons lying on the shortest path between the starting point and target point will be burst. Given the coordinates of the N balloons in an array arr, your task is to find out the maximum number of balloons that you can fire in one arrow shot.

---
+++ hit_most_balloons.py
@@ -0,0 +1,28 @@
+class Solution:
+    def mostBalloons(self, N, arr):
+        ans = 1
+        for i in range(N):
+            count = dict()
+            same_pos = 0
+            for j in range(N):
+                x_diff = arr[j][0] - arr[i][0]
+                y_diff = arr[j][1] - arr[i][1]
+                if x_diff == 0 and y_diff == 0:
+                    same_pos += 1
+                else:
+                    slope = float("inf") if y_diff == 0 else x_diff / y_diff
+                    count[slope] = count.get(slope, 0) + 1
+            ans = max(ans, max(count.values()) + same_pos)
+        return ans
+
+import math
+
+if __name__ == '__main__':
+    N = int(input())
+    arr = [[] for j in range(N)]
+    for j in range(2):
+        inp = [int(i) for i in input().split()]
+        for i in range(N):
+            arr[i].append(inp[i])
+    ob = Solution()
+    print(ob.mostBalloons(N, arr))
