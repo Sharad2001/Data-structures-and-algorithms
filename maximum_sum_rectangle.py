107502d06840860669c084fe9d66fd886ef0375f
Sharad2001 <vsharad2001@gmail.com>
2022-04-15 12:31:31 +0530

    Maximum Sum rectangle
    Given a 2D matrix M of dimensions RxC. Find the maximum sum submatrix in it.

---
+++ maximum_sum_rectangle.py
@@ -0,0 +1,30 @@
+class Solution:
+    def kadanes(self, arr, n):
+        s, maxi = arr[0], arr[0]
+        for i in range(1, n):
+            s += arr[i]
+            s = max(s, arr[i])
+            maxi = max(s, maxi)
+        return maxi
+
+    def maximumSumRectangle(self, R, C, M):
+        res = M[0][0]
+        for starti in range(R):
+            subMatSum = [0 for _ in range(C)]
+            for i in range(starti, R):
+                for j in range(C):
+                    subMatSum[j] += M[i][j]
+                res = max(res, self.kadanes(subMatSum, C))
+        return res
+
+import math
+import sys
+
+if __name__ == '__main__':
+    N, M = map(int, sys.stdin.readline().strip().split())
+    a = []
+    for i in range(N):
+        s = list(map(int, sys.stdin.readline().strip().split()))
+        a.append(s)
+    ob = Solution()
+    print(ob.maximumSumRectangle(N, M, a))
