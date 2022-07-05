897d27471e6d79b5227fa89dd2c353aa18227bd7
Sharad2001 <vsharad2001@gmail.com>
2022-07-05 15:21:55 +0530

    Diameter of a binary tree
    The diameter of a tree (sometimes called the width) is the number of nodes on the longest path between two end nodes. The diagram below shows two trees each with diameter nine, the leaves that form the ends of the longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes).

---
+++ diameter_of_a_binary_tree.py
@@ -0,0 +1,82 @@
+class Solution:
+    def __init__(self):
+        self.ans = 0
+
+    def height(self, root):
+        if root is None:
+            return 0
+        lh = self.height(root.left)
+        rh = self.height(root.right)
+        self.ans = max(self.ans, 1 + lh + rh)
+        return 1 + max(lh, rh)
+    def diameter(self, root):
+        self.height(root)
+        return self.ans
+
+from collections import deque
+import sys
+
+sys.setrecursionlimit(50000)
+
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+def buildTree(s):
+    # Corner Case
+    if (len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while (size > 0 and i < len(ip)):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if (currVal != "N"):
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if (i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        if (currVal != "N"):
+            currNode.right = Node(int(currVal))
+
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+if __name__ == "__main__":
+    s = input()
+    root = buildTree(s)
+    k = Solution().diameter(root)
+    print(k)
