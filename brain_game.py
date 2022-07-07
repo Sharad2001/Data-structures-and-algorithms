2c3f3362c1dcd847814e6df66ecdd31289cd6bdc
Sharad2001 <vsharad2001@gmail.com>
2022-07-07 13:20:07 +0530

    Brain Game
    2 players A and B take turns alternatively to play a game in which they have N numbers on a paper. In one turn, a player can replace one of the numbers by any of its factor (except for 1 & the number itself). The player who is unable to make a move looses the game. Find the winner of the game if A starts the game and both play optimally.

---
+++ brain_game.py
@@ -0,0 +1,22 @@
+class Solution:
+    def brainGame(self, nums):
+        x=[0]*1005
+        for i in range(2,1001):
+            for j in range(2*i,1001,i):
+                x[j]= max(x[j],1+x[i]);
+        b=0;
+        for i in range(len(nums)):
+            b=b^x[nums[i]];
+        if(b==0):
+           return False
+        return True
+
+if __name__ == '__main__':
+    n = int(input())
+    nums = list(map(int,input().split()))
+    ob = Solution()
+    ans = ob.brainGame(nums)
+    if(ans):
+        print("A")
+    else:
+        print("B")
