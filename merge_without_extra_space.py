5ba16d64493eafedabc9668f75014b84ed5613f5
Sharad2001 <vsharad2001@gmail.com>
2022-07-13 13:09:47 +0530

    Merge without extra space
    Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order without using any extra space. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.

---
+++ merge_without_extra_space.py
@@ -0,0 +1,26 @@
+class Solution:
+    def merge(self, arr1, arr2, n, m):
+        i = 0
+        j = 0
+        k = n - 1
+        while i <= k and j < m:
+            if arr1[i] < arr2[j]:
+                i += 1
+            else:
+                temp = arr2[j]
+                arr2[j] = arr1[k]
+                arr1[k] = temp
+                j += 1
+                k -= 1
+        arr1.sort()
+        arr2.sort()
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().strip().split())
+    arr1 = list(map(int, input().strip().split()))
+    arr2 = list(map(int, input().strip().split()))
+    ob = Solution()
+    ob.merge(arr1, arr2, n, m)
+    print(*arr1, end=" ")
+    print(*arr2)
